705e20d0535df7e11b9ac357e1fadb2f350cbe2e
keerthi <keerthi.official.007@gmail.com>
2024-05-30 22:35:46 +0530

    hostchange

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask_mysqldb import MySQL
 import io
 
 app = Flask(__name__)
-app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_HOST'] = '127.0.0.1'
 app.config['MYSQL_USER'] = 'root'
 app.config['MYSQL_PASSWORD'] = 'password123'
 app.config['MYSQL_DB'] = 'file_storage'
